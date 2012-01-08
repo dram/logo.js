869835804b40560f03be91423c7b206500774b6f
Xin Wang <dram.wang@gmail.com>
2012-01-08 13:10:20 +0800

    Accept minus character to input negatives.

--- logo-tile.js
+++ logo-tile.js
@@ -1460,7 +1460,7 @@ traits.SourceCanvas = Self.trait([], {
                 break
 	    case '0': case '1': case '2': case '3':
 	    case '4': case '5': case '6': case '7':
-	    case '8': case '9':
+	    case '8': case '9': case '-':
                 tile.text.content = tile.text.content.slice(0,-1) + event.key + '|'
 	        break
             }
